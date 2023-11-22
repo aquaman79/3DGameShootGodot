5ab17426d8ce98b02af88a58d1794b835c44e836
aquaman79 <qquamar1@gmail.com>
2023-11-22 17:21:26 +0100

    game over done1

--- Control.gd
+++ Control.gd
@@ -14,3 +14,7 @@ func _process(delta):
 func _on_button_2_pressed():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  # Rendre la souris visible
 	get_tree().change_scene_to_file("res://worldpascontinu1.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().quit()
