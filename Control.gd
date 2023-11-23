c83c6f4815e4ecaff221fc910173cb6a1a94808f
ABADA <fatima-ezzahra.abada@capgemini.com>
2023-11-23 16:04:03 +0100

    Version Finale Zombie Game fati

--- Control.gd
+++ Control.gd
@@ -13,7 +13,7 @@ func _process(delta):
 
 func _on_button_2_pressed():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  # Rendre la souris visible
-	get_tree().change_scene_to_file("res://worldpascontinu1.tscn")
+	get_tree().change_scene_to_file("res://worldpascontinu.tscn")
 
 
 func _on_button_3_pressed():
--- ene;y.gd
+++ ene;y.gd
@@ -2,7 +2,10 @@ extends CharacterBody3D
 
 
 const MOVE_SPEED = 3
- 
+const DETECTION_DISTANCE = 20  # Distance à laquelle le zombie détecte le joueur
+const FIELD_OF_VIEW = 45  # Angle de champ de vision en degrés
+const MIN_DISTANCE_TO_PLAYER = 1.5  # Définissez une distance minimale pour considérer que le joueur est attrapé
+
 var raycast 
 var anim_player 
  
@@ -18,26 +21,46 @@ func _ready():
 	add_to_group("zombies")
  
 func _physics_process(delta):
-	if dead:
+	if dead or player == null:
 		return
-	if player == null:
-		return
- 
-	var vec_to_player 
-	vec_to_player = player.global_transform.origin - global_transform.origin
-	vec_to_player = vec_to_player.normalized()
-	raycast.target_position = global_transform.origin + vec_to_player * 1.5
 
- 
-	move_and_collide(vec_to_player * MOVE_SPEED * delta)
- 
+	var vec_to_player = player.global_transform.origin - global_transform.origin
+	var distance_to_player = vec_to_player.length()
+	vec_to_player = vec_to_player.normalized()
+
+	# Mise à jour de la position cible du raycast
+	raycast.target_position = global_transform.origin + vec_to_player * 1.5
+	
+	# Vérifiez si la distance est suffisamment proche pour attraper le joueur
+	if distance_to_player <= MIN_DISTANCE_TO_PLAYER:
+		print("Game Over")
+		show_game_over_interface()
+		# Vous pouvez ajouter ici d'autres logiques, comme finir le jeu ou recharger la scène
+		return
+
+	if distance_to_player <= DETECTION_DISTANCE :
+		anim_player.play("walk")
+		move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	else:
+		anim_player.play("walk")
+		##move_and_collide(vec_to_player * MOVE_SPEED * delta)
+
 	if raycast.is_colliding():
 		var coll = raycast.get_collider()
 		if coll != null and coll.name == "Player" and !coll.is_shooting:
-			#emit_signal("zombie_hit_player")
 			kill()
 
- 
+
+
+func show_game_over_interface():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+	get_tree().change_scene_to_file("res://control.tscn")
+
+# Fonction pour vérifier si le joueur est dans le champ de vision du zombie
+func is_player_in_view(vec_to_player):
+	var direction_to_player = vec_to_player.normalized()
+	var forward_direction = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+	return forward_direction.dot(direction_to_player) > cos(deg_to_rad((FIELD_OF_VIEW / 2)))
  
 func kill():
 	#print("player")
